a1a53cfd89b4e803b7923ab476094d033f49b742
Serhii <sergiybiluk@gmail.com>
2020-05-30 21:19:43 +0200

    add tokio

--- src/client.rs
+++ src/client.rs
@@ -1,10 +1,12 @@
 use std::io::prelude::*;
 use std::io::Cursor;
 use std::mem::transmute;
-use std::net::{TcpListener, TcpStream};
 use std::str;
 use std::thread;
 use std::time::Duration;
+use tokio::io::{self, AsyncWriteExt, BufWriter};
+use tokio::net::TcpStream;
+use tokio::prelude::*;
 
 pub struct Client {
     connection: TcpStream,
@@ -16,40 +18,26 @@ const CRLF: &str = "\r\n";
 const PING: &str = "*1\r\n$4\r\nPING\r\n";
 
 impl Client {
-    pub fn new(host: &str) -> Client {
+    pub async fn new(host: &str) -> Result<Client, std::io::Error> {
         let address = format!("{}:6379", host);
-        let stream = TcpStream::connect(address).expect(CONNECTION_ERROR);
-        let connection = stream.try_clone().unwrap();
+        let connection = TcpStream::connect(address).await?;
 
-        connection
-            .set_read_timeout(Some(Duration::new(1, 0)))
-            .expect("Failed to set read timeout");
-
-        connection
-            .set_write_timeout(Some(Duration::new(1, 0)))
-            .expect("Failed to set write timeout");
-
-        println!("Test {}", PING);
-
-        Client { connection }
+        Ok(Client { connection })
     }
 
-    pub fn send(&mut self, command: &str) -> Result<String, std::io::Error> {
+    pub async fn send(&mut self, command: &str) -> Result<String, std::io::Error> {
         let result = command.as_bytes();
 
-        match self.connection.write(result) {
-            Ok(res) => println!("SEND {:?}", res),
-            Err(err) => println!("Error: send. {}", err),
-        };
+        self.connection.write(result).await?;
 
         let mut buffer: Vec<u8> = Vec::with_capacity(20);
         buffer.extend_from_slice(&[0; 50]);
 
-        match self.connection.read(&mut buffer) {
+        match self.connection.read(&mut buffer).await {
             Ok(_) => {
-                let mut s = String::from("");
-                s.push_str(&String::from_utf8_lossy(&buffer));
-                Ok(s)
+                let mut data = String::from("");
+                data.push_str(&String::from_utf8_lossy(&buffer));
+                Ok(data)
             }
             Err(e) => Err(e),
         }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,41 @@
 use std::io::prelude::*;
 use std::io::Cursor;
 use std::mem::transmute;
-use std::net::{TcpListener, TcpStream};
 use std::str;
 use std::thread;
 use std::time::Duration;
-
+// use tokio::net::TcpListener;
+use tokio::prelude::*;
+use tokio::sync::mpsc::{channel, Receiver, Sender};
+use tokio::time::{delay_for, Delay};
 mod client;
 use client::Client;
 
-fn main() {
-    let mut connection = Client::new("127.0.0.1");
-    let command = "*2\r\n$3\r\nget\r\n$3\r\nfoo\r\n";
-    let result = connection.send(command);
-    println!("Result {:?}", result);
+fn sleep(ms: u64) -> Delay {
+    delay_for(Duration::from_millis(ms))
+}
+enum Message {
+    Foo,
+    Bar,
+}
+
+async fn send_message(mut channel: Sender<Message>) {
+    match channel.send(Message::Bar).await {
+        Ok(_) => println!("Hello"),
+        Err(e) => println!("Hello"),
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let mut connection = Client::new("127.0.0.1").await;
+    match connection {
+        Ok(mut conn) => {
+            let command = "*2\r\n$3\r\nget\r\n$3\r\nfoo\r\n";
+            let result = conn.send(command).await;
+            sleep(1000).await;
+            println!("Result {:?}", result);
+        }
+        Err(e) => println!("Error"),
+    }
 }
