2b409a7aa02f6324a3f143a5007fe49419d690ae
Serhii <sergiybiluk@gmail.com>
2020-05-31 23:27:31 +0200

    add comment

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod parser;
 use client::Client;
 use parser::RESP;
 
+/// see https://github.com/tokio-rs/mini-redis/blob/master/src/client.rs as simple tokio redis example
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let mut connection = Client::new("127.0.0.1").await?;
